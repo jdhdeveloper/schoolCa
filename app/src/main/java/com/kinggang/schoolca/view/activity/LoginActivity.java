17110dd4c62e569e7bcb984831f24384d70daa05
jdhdevelop <18227754727@163.com>
2017-03-03 14:02:47 +0800

    登录界面绘制

--- app/src/main/java/com/kinggang/schoolca/view/activity/LoginActivity.java
+++ app/src/main/java/com/kinggang/schoolca/view/activity/LoginActivity.java
@@ -17,34 +17,4 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (keyCode==KeyEvent.KEYCODE_BACK){
-//            ToQuitTheApp();
-//            return false;
-//        }else{
-//            return super.onKeyDown(keyCode, event);
-//        }
-//    }
-//
-//    //封装ToQuitTheApp方法
-//    private void ToQuitTheApp() {
-//        if (isExit) {
-//            finish();
-//            System.exit(0);
-//        } else {
-//            isExit = true;
-//            Toast.makeText(LoginActivity.this, "再按一次退出APP", Toast.LENGTH_SHORT).show();
-//            mHandler.sendEmptyMessageDelayed(0, 2000);// 3秒后发送消息
-//        }
-//    }
-//    //创建Handler对象，用来处理消息
-//    private Handler mHandler = new Handler() {
-//
-//        @Override
-//        public void handleMessage(Message msg) {//处理消息
-//            super.handleMessage(msg);
-//            isExit = false;
-//        }
-//    };
 }
